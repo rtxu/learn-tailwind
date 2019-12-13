71e74fc247c12928d9ea40acb590cea2d3a53653
ruitao.xu <ruitao.xu@aliyun.com>
2019-12-13 18:38:31 +0800

    chore: add postcss config
    ref: https://github.com/postcss/postcss#usage

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,11 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
+        exclude: /node_modules/,
         use: [
-          'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          'postcss-loader'
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "postcss-loader"
         ]
       }
     ]
