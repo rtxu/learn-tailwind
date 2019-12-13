917cdb2cfd2b9e428b36c3b627501c44ee85bf31
ruitao.xu <ruitao.xu@aliyun.com>
2019-12-13 18:35:13 +0800

    chore: introduce webpack loaders style-loader/css-loader/postcss-loader based on webpack doc
    ref: https://webpack.js.org/loaders/postcss-loader/

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
+      }
+    ]
   }
 };
