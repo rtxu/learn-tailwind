d551115329a42f8a16f1825e419049b5b1f824d8
ruitao.xu <ruitao.xu@aliyun.com>
2019-12-16 15:36:13 +0800

    chore: setup react dev env
    ref: https://blog.usejournal.com/creating-a-react-app-from-scratch-f3c693b84658

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
-
-function component() {
-  const element = document.createElement("div");
-
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  return element;
-}
-
-document.body.appendChild(component());
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+ReactDOM.render(<App />, document.getElementById("root"));
